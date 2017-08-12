d1eafb81c07929d573a25a4e043ed2c74fe3c37c
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2017-08-13 00:39:32 +0100

    Simple rest endpoint in golang

--- src/ka.org/sample/alert2event/main/main.go
+++ src/ka.org/sample/alert2event/main/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"ka.org/sample/alert2event/app"
 	"os"
+	"fmt"
 )
 
 func main() {
 	eventApi := os.Getenv("EVENTAPI_URL")
 	if len(eventApi) == 0 {
-		panic("Env var EVENTAPI_URL is not defined. Define the var and rerun the app")
+		fmt.Println("Env var EVENTAPI_URL is not defined. Define the var and rerun the app")
+		os.Exit(1)
 	}
 
 	server := app.App{Port: 8080, EventApiUrl: eventApi}
